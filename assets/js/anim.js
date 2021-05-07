090799f79bb21a57e0cbda903e1bbacdb21da2e9
Kresten24 <ronie-8@hotmail.com>
2021-05-07 12:22:34 -0400

    old portfolio

---
+++ assets/js/anim.js
@@ -0,0 +1,14 @@
+$(function () {
+  var skill = [$("#Skill-Git"), $("#Skill-HTML-CSS"), $("#Skill-Bootstrap"), $("#Skill-JavaScript"), $("#Skill-Jquery"), $("#Skill-Java"), $("#Skill-Python"), $("#Skill-PHP"), $("#Skill-NodeJS"),
+  $("#Skill-MySql"), $("#Skill-Ruby"), $("#Skill-MongoDB"), $("#Skill-NPM"), $("#Skill-Cloud9"), $("#Skill-Blender"), $("#Skill-UnitTests"), $("#Skill-English"), $("#Skill-French"),
+  $("#Skill-Armenian"), $("#Skill-Arabic"), $("#Skill-Turkish"), $("#Skill-Photoshop"), $("#Skill-Illustrator"), $("#Skill-Moqups")];
+  var skillClass = ["git", "htmlcss", "bootstrap","js", "jq", "java", "python", "php", "nodejs", "mysql", "ruby", "mongodb", "npm", "cloud9", "blender", "unitTests", "english", "french", "armenian", "arabic", "turkish",
+"photoshop", "illustrator", "moqups"];
+  $(window).scroll(function () {
+    if($(window).scrollTop() > 0) {
+      for(var i=0; i<=skill.length; i++){
+        skill[i].addClass(skillClass[i]);
+      }
+    }
+  });
+});
